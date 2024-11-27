7ba13039a13e1478f2acb9d411f1c8452f5b65d8
GulnurMirzayeva <gmirzyeva1@gmail.ru>
2024-11-27 20:06:27 +0400

    Update .gitignore

--- app/Http/Controllers/Admin/AdminController.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Http\Controllers\Admin;
-use App\Http\Controllers\Controller;
-
-class AdminController extends Controller {
-    public function index() {
-        return view('admin.layouts.admin');
-    }
-}
